10593de96641dbe46d422f02f58afbdcccc7c88c
Monma_Takeshi <jemini0527@gmail.com>
2020-06-14 20:24:53 +0900

    マイグレーションファイル記述 usersテーブル
    usersテーブルのマイグレーションファイルに作成するカラムの情報を記述しました。

--- db/migrate/20200614092142_devise_create_users.rb
+++ db/migrate/20200614092142_devise_create_users.rb
@@ -4,8 +4,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.date   :birth_on
+      t.text   :profile
+      t.string :image
 
       ## Recoverable
       t.string   :reset_password_token
@@ -36,6 +40,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.timestamps null: false
     end
 
+    add_index :users, :nickname
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true

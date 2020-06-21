1945190016f44d528e29060bb19ce875a22a156d
Monma_Takeshi <jemini0527@gmail.com>
2020-06-21 20:30:10 +0900

    factory bot導入 usersインスタンス定義
    specディレクトリ直下にfactoriesディレクトリを作成。直下にusers.rbファイルを作成し、ユーザー登録時に使用するインスタンス情報を定義しました。

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+
+  factory :user do
+    nickname              {"太郎"}
+    email                 {"test@examplemail.com"}
+    password              {"sample0000"}
+  end
+
+end

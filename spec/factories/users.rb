dba3f7c23f8e1aea9f69f9dbb05d9978466c2e0c
Monma_Takeshi <jemini0527@gmail.com>
2020-06-22 19:25:31 +0900

    factorybot userインスタンスnickname変更
    factorybotのuserインスタンスでnicknameを２→５文字に変更。
    ※2文字だと登録できる境界値ギリギリのため

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
 
   factory :user do
-    nickname              {"太郎"}
+    nickname              {"テスト太郎"}
     email                 {"test@examplemail.com"}
     password              {"sample0000"}
   end

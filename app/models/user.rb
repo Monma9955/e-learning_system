264e2eae8579c0848096583a8fad5f96aaebb5b6
Monma_Takeshi <jemini0527@gmail.com>
2020-06-23 00:35:41 +0900

    バリデーション設定 userモデル
    userモデルにバリデーション設定。
    nickname、email、passwordカラムにそれぞれバリデーションを設定しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true, uniqueness: true
+  validates :nickname, presence: true, uniqueness: true, length: { in: 2..14 }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, presence: true, length: { in: 8..100 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i }
 end

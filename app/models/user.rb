6a37c9261d690cde9b4eca0dbb9c402eb5332b9e
Monma_Takeshi <jemini0527@gmail.com>
2020-06-24 00:19:58 +0900

    devise日本語化 テスト修正
    deviseの日本語化に伴い、テスト時に吐き出すメッセージを日本語化したdeviseのメッセージと合わせました。
    ※deviseがバリデーションのカラムに対応していなかったため、日本語化ファイルも合うように修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
 
   validates :nickname, presence: true, uniqueness: true, length: { in: 2..14 }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }

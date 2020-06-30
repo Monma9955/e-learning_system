bb836b9f789eb841f63b9ce84d49fea9c5e9af9d
Monma_Takeshi <jemini0527@gmail.com>
2020-07-01 00:08:09 +0900

    Categoriesテーブル作成 migrate
    マイグレーションファイルにname、ancestryカラムを追加し、rake db:migrateコマンドでCategoriesテーブルを作成しました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_14_092142) do
+ActiveRecord::Schema.define(version: 2020_06_30_111854) do
+
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "ancestry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false

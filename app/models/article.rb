d9db2cf42c816fc065361dced1259931b55a4098
Monma_Takeshi <jemini0527@gmail.com>
2020-07-19 00:26:52 +0900

    enumのハッシュ作成 article.rb
    articleモデルのformatカラムにenum型のハッシュを記載しました。
    後ほどi18nで日本語化予定のため、ハッシュのキーは英語表記にしました。

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :category, optional: true
   has_many :pages, dependent: :destroy
+
+  enum format:{only_one: 0, multiple: 1}
+  # 択一問題=only_one、 複数選択問題=multiple
 end

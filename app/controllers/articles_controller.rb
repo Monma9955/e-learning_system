c4161596e7a03266b46c6cfdac2e6325f61e4280
Monma_Takeshi <jemini0527@gmail.com>
2020-07-24 19:34:00 +0900

    createメソッド作成 articles_controller.rb
    articles_controllerにcreateメソッドを作成。
    インスタンスが保存できた場合とできなかった場合で処理を分けました。

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,14 @@ class ArticlesController < ApplicationController
     @root_categories = Category.root
   end
 
+  def create
+    @article = FormArticle.new(article_params)
+    if @article.save
+      redirect_to root_path, notice: "問題を作成しました"
+    else
+      flash.now[:alert] = "問題の作成に失敗しました"
+      render :new
+    end
+  end
+
 end

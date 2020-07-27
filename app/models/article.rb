89d4b48c9f1f70540c59a932ef9dc6577907ad6f
Monma_Takeshi <jemini0527@gmail.com>
2020-07-28 00:57:55 +0900

    enum 表記微調整 article.rb
    enumの日本語表記訳の位置を微調整しました。

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,5 @@ class Article < ApplicationRecord
   has_many :pages, dependent: :destroy
 
   enum format:{only_one: 0, multiple: 1}
-  # 択一問題=only_one、 複数選択問題=multiple
+  # only_one = 択一問題、 multiple = 複数選択問題
 end

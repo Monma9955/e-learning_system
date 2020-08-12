ba6b4b0e0cc2059cdb890152f08f3496348b406d
Monma_Takeshi <jemini0527@gmail.com>
2020-08-12 18:43:44 +0900

    form_article単体テスト バリデーション記載
    form_article.rbの各カラムにバリデーションを記載しました。
    また、article_category カラムのデータ型はintegerに修正しました。

--- app/forms/form_article.rb
+++ app/forms/form_article.rb
@@ -4,7 +4,7 @@ class FormArticle
   include ActiveModel::Validations
 
   attribute :created_user_id, :integer
-  attribute :article_category, :string
+  attribute :article_category, :integer
   attribute :article_format, :integer
   attribute :article_title, :string
   attribute :page_text, :string
@@ -14,14 +14,15 @@ class FormArticle
   attribute :article_time_limit, :integer
   attribute :article_public, :boolean
 
-  validates :article_category, presence: true
-  validates :article_format, presence: true
-  validates :article_title, presence: true
-  validates :page_text, presence: true
-  validates :option_correct, presence: true
-  validates :option_wrong, presence: true
+  validates :created_user_id, presence: true, numericality: { only_integer: true }
+  validates :article_category, presence: true, inclusion: 1..165
+  validates :article_format, presence: true, inclusion: 0..1
+  validates :article_title, presence: true, length: { maximum: 30 }
+  validates :page_text, presence: true, length: { maximum: 1000 }
+  validates :option_correct, presence: true, length: { maximum: 200 }
+  validates :option_wrong, presence: true, length: { maximum: 200 }
   validates :article_price, inclusion: 100..999_999, allow_nil: true
-  validates :article_time_limit, inclusion: 0..120, allow_nil: true
+  validates :article_time_limit, inclusion: 1..120, allow_nil: true
 
   def save
     return false unless valid?

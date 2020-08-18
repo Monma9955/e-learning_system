31f4721a313ba9798dac7d7fa55a26ee839696b1
Monma_Takeshi <jemini0527@gmail.com>
2020-08-18 15:21:17 +0900

    Articleコントローラーのテスト POST #create
    問題を投稿した際の動作テスト。
    投稿に成功した場合の各テーブルの変化と遷移先をテストしました。
    投稿に失敗した場合はテーブルが変化しないこととnew.html.hamlに遷移することをテストしました。
    ログインしていない場合はログイン画面にリダイレクトすることをテストしました。

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -35,4 +35,75 @@ describe ArticlesController do
 
   end
 
+  describe 'POST #create' do
+    let(:params) { { created_user_id: user.id, form_article: attributes_for(:form_article) } }
+
+    context 'ログインしている場合' do
+
+      before do
+        login user
+        create(:category)
+      end
+
+      context '投稿に成功した場合' do
+        subject {
+          post :create,
+          params: params
+        }
+
+        it "articleを保存すること" do
+          expect{ subject }.to change(Article, :count).by(1)
+        end
+
+        it "pageを保存すること" do
+          expect{ subject }.to change(Page, :count).by(1)
+        end
+
+        it "optionを保存すること" do
+          expect{ subject }.to change(Option, :count).by(2)
+        end
+
+        it "root_pathへリダイレクトすること" do
+          subject
+          expect(response).to redirect_to(root_path)
+        end
+      end
+
+      context '投稿に失敗した場合' do
+        let(:invalid_params) { { created_user_id: user.id, form_article: attributes_for(:form_article, article_title: nil) } }
+
+        subject {
+          post :create,
+          params: invalid_params
+        }
+
+        it "Articleを保存しないこと" do
+          expect{ subject }.not_to change(Article, :count)
+        end
+
+        it "Pageを保存しないこと" do
+          expect{ subject }.not_to change(Page, :count)
+        end
+
+        it "Optionを保存しないこと" do
+          expect{ subject }.not_to change(Option, :count)
+        end
+
+        it "new.html.hamlに遷移すること" do
+          subject
+          expect(response).to render_template :new
+        end
+      end
+
+    end
+
+    context 'ログインしていない場合' do
+
+      it "ログイン画面にリダイレクトすること" do
+        post :create, params: params
+        expect(response).to redirect_to(new_user_session_path)
+      end
+
+    end
+  end
 end

cd26d46bed2cdaaac6111cc6a99031a658948683
Monma_Takeshi <jemini0527@gmail.com>
2020-09-04 01:08:55 +0900

    rootパス変更 routes.rb
    トップページをcategoriesコントローラーのindexビューに変更しました。
    ビューはこれから作成。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'articles#index'
+  root to: 'categories#index'
   resources :articles, only: [:new, :create] do
     collection do
       get 'get_category_parents', defaults: { format: 'json' }

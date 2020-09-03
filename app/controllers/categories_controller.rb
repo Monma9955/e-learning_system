722d463c14dc8cc70791d2135080b26d545c442a
Monma_Takeshi <jemini0527@gmail.com>
2020-09-04 01:12:46 +0900

    ルートカテゴリ取得 categories#index
    カテゴリコントローラーのindexアクションでルートカテゴリの一覧が使用できるようにしました。

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,7 @@
 class CategoriesController < ApplicationController
+  before_action :set_root_categories, only: [:index]
+
+  def set_root_categories
+    @root_categories = Category.root
+  end
 end

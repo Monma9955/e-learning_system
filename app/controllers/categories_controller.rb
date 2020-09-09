1efc5097a256c47a4e3ac421349efe62856ccfb7
Monma_Takeshi <jemini0527@gmail.com>
2020-09-10 01:49:40 +0900

    categories#show作成
    categoriesコントローラーのshowアクションを作成。
    ルーティングを組んでshow.html.hamlが表示されるようにしました。
    show.html.hamlの内容は以前のindex.html.hamlと同一のためコピー＆ペーストで実装。

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,9 @@
 class CategoriesController < ApplicationController
-  before_action :set_root_categories, only: [:index]
+  before_action :set_root_categories, only: [:index, :show]
+
+  def show
+    @current_category_children = Category.find(params[:id]).children
+  end
 
   def set_root_categories
     @root_categories = Category.root
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'categories#index'
+
+  resources :categories, only: [:index, :show]
   resources :articles, only: [:new, :create] do
     collection do
       get 'get_category_parents', defaults: { format: 'json' }

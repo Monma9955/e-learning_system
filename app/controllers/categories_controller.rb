9179ee5c70b8a94d19b596ad92f211fe4a37bae7
Monma_Takeshi <jemini0527@gmail.com>
2020-09-16 01:25:16 +0900

    インスタンス変数作成 categories_controller.rb
    categories#show_articlesに渡すインスタンス変数を作成しました。

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,15 @@
 class CategoriesController < ApplicationController
-  before_action :set_root_categories, only: [:index, :show]
+  before_action :set_root_categories, only: [:index, :show, :show_articles]
 
   def show
     @current_category = Category.find(params[:id])
     @current_category_children = @current_category.children
+  end
+
+  def show_articles
+    @current_root_category = Category.find(params[:category_id])
+    @current_root_category_children = @current_root_category.children
+    @current_parent_category = Category.find(params[:id])
   end
 
   def set_root_categories
